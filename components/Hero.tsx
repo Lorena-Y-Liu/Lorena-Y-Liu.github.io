2b8e24239cbb43ebe5d129c19d172a99f7a2836f
LiuYuhui <153437088+LorenaLyu@users.noreply.github.com>
2025-11-24 16:04:00 +0800

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,7 +25,7 @@ const Hero: React.FC = () => {
               scaleOnHover={1.05}
             >
                <img 
-                 src="/public/profile.jpg" 
+                 src="public/profile.jpg" 
                  alt="Yuhui LIU"
                  className="object-cover w-full h-full"
                />
